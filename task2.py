4fbb82ef090db42282bc6b2cbbb5a524d2aabcd0
yaroslavchumachenko <yar0den05@gmail.com>
2023-10-25 20:42:43 +0300

    innitial commit

---
+++ task2.py
@@ -0,0 +1,6 @@
+#this function will display "Hello world!";
+print("Hello ", "world", "!", sep="")
+#this function will display "sep", pass 4 lines and print "end" with gap in the end;
+print("sep", "end", sep="\n\n\n\n", end="\n\n")
+#this function will display "end" with 4 lines after that;
+print("end", end="\n\n\n\n")
