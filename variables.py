4dace6c29652814699a2936d8acf5e986349ff09
yaroslavchumachenko <yar0den05@gmail.com>
2023-10-28 16:54:36 +0300

    add new theme

---
+++ variables.py
@@ -0,0 +1,12 @@
+#Існують такі типи даних як string, float, integer(int), bool
+my_string = "Hello Git!"
+my_number = 27
+my_boolean = True
+#Для перенесення строк використовується \
+my_long_string = "toooooooooooooooooooooooooooo loooooooooooo\
+    ong sring"
+#Можна не використовувати \ для переноси 
+triple_string = """hello
+git
+hub"""
+# / - ділення, // ділення без остачі, % ділення із залишком 
