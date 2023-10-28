67b9032cd6efab7f61c533bdb0e9dc65694eaa2e
yaroslavchumachenko <yar0den05@gmail.com>
2023-10-28 19:09:16 +0300

    innitial commit

---
+++ practical3.py
@@ -0,0 +1,15 @@
+id_text = None
+
+id_text = input("Введіть Transaction ID:")
+
+id_text = id_text[1:]
+id_text = id_text[:-1]
+print(id_text)
+
+
+#for element in range(len(id_text)):
+#    element += 1
+#    if element == ";" or element == "%":
+#        pass
+#    else:
+#        print(element)
