923612fa1e4b00b5938d0b76d8976cd7a8cee182
yaroslavchumachenko <yar0den05@gmail.com>
2023-10-30 19:43:58 +0200

    initial commit

---
+++ input.py
@@ -0,0 +1,24 @@
+#ask a user about something
+user_name = input("Print there your name: ")
+#import use peps.python.org
+#the right way to import
+import os
+import sys
+#the wrong way
+import sys, os 
+#import of your own moduls
+import os
+#import files_for_practice (you need to create file __init__)!!!!
+#if you have this file, you can call it a "package"
+#from files_for_practice import simple
+#you should use prioritization of libraries import os -> import file -> from os -> from file
+import sys
+from sys import path
+path("User/Windows...")
+#BUT if you didnt connect specific function, you should write the name of library:
+import sys
+sys.path("...")
+import random
+random.randint()
+#python framework - like Django
+#framework >>> package or library
